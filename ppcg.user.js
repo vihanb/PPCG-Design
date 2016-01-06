59ff5fb08c9f4aadd4ea8c1b8a4e83140a2adcf3
Vihan Bhargava <blue1086@gmail.com>
2016-01-05 16:30:40 -0800

    added option button!

--- ppcg.user.js
+++ ppcg.user.js
@@ -16,11 +16,13 @@ window.addEventListener("load",function(){
   // CONSTANTS
   var FAVICON = "//i.stack.imgur.com/jOhpI.png"; // Favicon
   var BACKGROUND = "//i.stack.imgur.com/t8GhU.png"; // Background Texture
+  var SPRITES = "https://cdn.rawgit.com/vihanb/PPCG-Design/master/sprites.svg"; // Sprites
   
   if((window.location+"").search("//codegolf.stackexchange.com")>=0){
     qS("link[rel$=\"icon\"]").href = FAVICON;
     var x = qS(".beta-title").parentElement;
     qS(".beta-title").parentElement.removeChild(qS(".beta-title"));
+    $(".topbar-wrapper > .network-items").append('<a id="USER_Opt" class="topbar-icon yes-hover" style="width: 36px; background-image: url('+SPRITES'); background-position: 0px 0px;"></a>');
     $("#content").css('background', 'none');
     $(".post-tag").css('background', 'rgb(177, 235, 124)');
     $(".post-tag").css('color', '#14761a');
@@ -28,7 +30,7 @@ window.addEventListener("load",function(){
     $("#mainbar, .user-page #content").css('background', '#FAFAFA');
     $("#mainbar").css('padding', '15px');
     $(".envelope-on, .envelope-off, .vote-up-off, .vote-up-on, .vote-down-off, .vote-down-on, .star-on, .star-off, .comment-up-off, .comment-up-on, .comment-flag, .edited-yes, .feed-icon, .vote-accepted-off, .vote-accepted-on, .vote-accepted-bounty, .badge-earned-check, .delete-tag, .grippie, .expander-arrow-hide, .expander-arrow-show, .expander-arrow-small-hide, .expander-arrow-small-show, .anonymous-gravatar, .badge1, .badge2, .badge3, .gp-share, .fb-share, .twitter-share, #notify-container span.notify-close, .migrated.to, .migrated.from")
-        .css("background-image", "url(https://cdn.rawgit.com/vihanb/PPCG-Design/master/sprites.svg)");
+        .css("background-image", "url("+SPRITES+")");
     $(".answer pre code").each(function() {
         var t=$(this).text().trim().replace(/\r\n/g, "\n");
         $(this).parent().before('<div style="font:11px \'Open Sans\'">'+(2*t.length-t.replace(/[\u0100-\uFFFF]/g,'').length)+" bytes</div>");
