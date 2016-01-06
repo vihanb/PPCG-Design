930fa82e2fc2b98365bafe678da6a9c82cf53d6a
Vihan Bhargava <blue1086@gmail.com>
2016-01-05 16:03:12 -0800

    logo should appear everywhere

--- ppcg.user.js
+++ ppcg.user.js
@@ -17,7 +17,6 @@ window.addEventListener("load",function(){
     qS("link[rel$=\"icon\"]").href = FAVICON;
     var x = qS(".beta-title").parentElement;
     qS(".beta-title").parentElement.removeChild(qS(".beta-title"));
-    $("[class*=favicon-codegolf]").css('background', 'url('+FAVICON+')').css('background-position', 'inital').css('background-size', '100% 100%');
     $("#content").css('background', 'none');
     $(".post-tag").css('background', 'rgb(177, 235, 124)');
     $(".post-tag").css('color', '#14761a');
@@ -31,7 +30,16 @@ window.addEventListener("load",function(){
         $(this).parent().before('<div style="font:11px \'Open Sans\'">'+(2*t.length-t.replace(/[\u0100-\uFFFF]/g,'').length)+" bytes</div>");
     });
     x.innerHTML = "<table id=\"newlogo\"><tr><td><img src=\""+FAVICON+"\" height=50></td><td>Programming Puzzles &amp; Code Golf</td></tr></table>";
-    document.head.innerHTML += "<style>@import url(https://fonts.googleapis.com/css?family=Open+Sans:400,700,700italic,400italic);@import url(https://fonts.googleapis.com/css?family=Exo+2:700);.module:not(.community-bulletin,#questions-count){background-color:rgb(250, 250, 250) !important;padding:10px !important;}.youarehere{color:rgb(72,125,75) !important;border-bottom:2px solid rgb(72,125,75) !important;}.bounty-indicator-tab{background:rgb(193, 225, 183);color:rgb(72,125,75) !important;}html,body{font-family:\"Open Sans\"}#header *{color:#4C4C4C;}#content{background:#FAFAFA}#newlogo{font-family:\"Exo 2\";top:-15px;position:relative;}#newlogo td{padding-right:15px;}#hlogo a{width:600px;}</style>";
+    document.head.innerHTML += "<style>@import url(https://fonts.googleapis.com/css?family=Open+Sans:400,700,700italic,400italic);@import url(https://fonts.googleapis.com/css?family=Exo+2:700);"+
+    ".module:not(.community-bulletin,#questions-count){background-color:rgb(250, 250, 250) !important;padding:10px !important;}"+
+    ".youarehere{color:rgb(72,125,75) !important;border-bottom:2px solid rgb(72,125,75) !important;}"+
+    "[class*=favicon-codegolf]{background:url("+FAVICON+");background-position:inital;background-size:100% 100%;}"+
+    ".bounty-indicator-tab{background:rgb(193, 225, 183);color:rgb(72,125,75) !important;}"+
+    "html,body{font-family:\"Open Sans\"}#header *{color:#4C4C4C;}"+
+    "#content{background:#FAFAFA}"+
+    "#newlogo{font-family:\"Exo 2\";top:-15px;position:relative;}"+
+    "#newlogo td{padding-right:15px;}"+
+    "#hlogo a{width:600px;}</style>";
     qS(".container").style.boxShadow = "none";
     $("body .container").prepend('<div style="position: absolute;width: inherit; height: 120px; background: linear-gradient(rgba(153, 255, 165, 0.26), rgba(140, 255, 149, 0.26)), url(http://cdn.sstatic.net/codereview/img/pattern.png?v=8286dee84d00)"></div>');
     if (CR_THEME) {
