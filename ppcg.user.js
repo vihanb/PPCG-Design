4072bcea499e8c2f05a232da489ac88f03b16760
ETHproductions <eth3792@nycap.rr.com>
2016-01-08 00:25:54 -0500

    Minor bug fix

--- ppcg.user.js
+++ ppcg.user.js
@@ -166,7 +166,7 @@ if(/^https?:\/\/(?:meta.)?codegolf.stackexchange.com/.test(window.location)) {
         answers = json.map(function(i) {
           i.body = i.body.replace(/^(?!<p><strong>|<h\d>)(.(?!<p><strong>|<h\d>))*/,"").replace(/<(strike|s|del)>.*<\/\1>/g,"").replace(/<a [^>]+>(.*)<\/a>/g,"$1").replace(/\(\s*(\d+)/g,", $1").replace(/\s*-\s+|:\s*/,", ");
           var j=+(i.body.match(/^\s*(?:<h\d>|<p><strong>).*?(\d+)\D*?<\/(?:h\d|strong)>/)||[0,"Score N/A"])[1];
-          i.body = i.body.replace(RegExp(",?\\s*"+j+".*"),"");
+          i.body = i.body.replace(RegExp(",?\\s+"+j+".*"),"");
           return [j, i];
         });
         var lv = 0;
