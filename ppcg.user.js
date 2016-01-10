640e40eed58ce6c549b728e6d9cb452abf20d10e
Benjamin Pang <molarmanful@users.noreply.github.com>
2016-01-10 08:40:17 -0800

    fixing link colors

--- ppcg.user.js
+++ ppcg.user.js
@@ -238,7 +238,7 @@ if(/^https?:\/\/(?:meta.)?codegolf.stackexchange.com/.test(window.location)) {
 	$("a").css('color', '#5DA261');
 	$("a:visited").css('color', '#254127');
 	$("#tabs a:hover").css({'color':'#5DA261 !important','border-bottom':'2px solid #5DA261 !important'});
-	$(".started a:not(.started-link), .started .started-link:hover").css('color','#487D4B');
+	$(".started a:not(.started-link)").css('color','#487D4B');
 	$("body .container").prepend('<div style="position: absolute;width: inherit; height: 120px; background: '+(localStorage.getItem('main.BACKGROUND_LIGHT')==="true"?'':main.BACKGROUND_TINT+', ')+ 'url('+main.BACKGROUND_IMAGE+')"></div>');
   }
   window.addEventListener("load",function(){
