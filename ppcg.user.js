95a587a92927c1acc3f8205f86ce9390cffb29c4
Vihan Bhargava <blue1086@gmail.com>
2016-01-31 13:36:02 -0800

    more chatt

--- ppcg.user.js
+++ ppcg.user.js
@@ -235,6 +235,9 @@ if ((window.location + "").search("//chat.stackexchange.com") >= 0) {
 
     '.message:hover .action-link, .message:hover .action-link .img.menu { background-color: #F3F3F3 !important }' +
     '.message:hover .action-link .img.menu { background-image: url(http://i.stack.imgur.com/3gBKh.png) !important; background-size: 16px 16px; background-position: 0px -1px !important; }' +
+    
+    '.monologue { margin-bottom: 0; padding-top: 0; }' +
+    '.messages { background-color: #fff; padding: 8px 8px 8px 0px; border-radius: 0; border-top: none }' +
 
     '.popup { border-radius: 2px; border: none; box-shadow: 0 0 8px #9C9C9C }' +
     
