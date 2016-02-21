1c6a9536d57bce4aa8d5869cf53654f0513d66dc
somebody1234 <somebody1234@users.noreply.github.com>
2016-02-21 20:15:51 +1000

    Increment version number

--- ppcg.user.js
+++ ppcg.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        PPCG Graduation Script
 // @namespace   https://github.com/vihanb/PPCG-Design
-// @version     3.6.1
+// @version     3.6.2
 // @description A script to self-graduate PPCG
 // @match       *://*.codegolf.stackexchange.com/*
 // @match       *://chat.stackexchange.com/*
