56d4cdb8212a3164e68ef237828bc976a892fd60
Ryan R <Mego@users.noreply.github.com>
2016-11-10 05:33:27 -0600

    Bump version

--- ppcg.user.js
+++ ppcg.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        PPCG Graduation Script
 // @namespace   https://github.com/vihanb/PPCG-Design
-// @version     3.8.6
+// @version     3.8.7
 // @description A script to self-graduate PPCG
 // @match       *://*.codegolf.stackexchange.com/*
 // @match       *://chat.stackexchange.com/*
