db490a9575b1fae83ffd041a2d687bb6ff27a87e
ETHproductions <eth3792@nycap.rr.com>
2016-12-02 13:02:54 -0500

    Increment version

--- ppcg.user.js
+++ ppcg.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        PPCG Graduation Script
 // @namespace   https://github.com/vihanb/PPCG-Design
-// @version     3.8.9
+// @version     3.8.10
 // @description A script to self-graduate PPCG
 // @match       *://*.codegolf.stackexchange.com/*
 // @match       *://chat.stackexchange.com/*
