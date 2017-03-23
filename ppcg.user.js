932733044f8272b500ed42dd98a57d79c0b5f3fc
ETHproductions <eth3792@nycap.rr.com>
2017-03-23 13:35:19 -0400

    Fix #114

--- ppcg.user.js
+++ ppcg.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        PPCG Graduation Script
 // @namespace   https://github.com/vihanb/PPCG-Design
-// @version     3.10.4
+// @version     3.10.5
 // @description A script to self-graduate PPCG
 // @match       *://*.codegolf.stackexchange.com/*
 // @match       *://codegolf.meta.stackexchange.com/*
@@ -9,6 +9,7 @@
 // @author      PPCG Community
 // @grant       GM_getValue
 // @grant       GM_setValue
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @updateURL   https://rawgit.com/vihanb/PPCG-Design/master/ppcg.user.js
 // ==/UserScript==
 ////////////////////////////////////////////////////////////////////////
