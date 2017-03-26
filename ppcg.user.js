c462822170e93c4bdfa948755610c01ebd4aa013
Vihan <vihanb@users.noreply.github.com>
2017-03-26 12:26:17 -0700

    Update ppcg.user.js

--- ppcg.user.js
+++ ppcg.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        PPCG Graduation Script
 // @namespace   https://github.com/vihanb/PPCG-Design
-// @version     3.10.6
+// @version     3.10.7
 // @description A script to self-graduate PPCG
 // @match       *://*.codegolf.stackexchange.com/*
 // @match       *://codegolf.meta.stackexchange.com/*
