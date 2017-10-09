427f619d368553f9fc15e3ba80af4ef2ab82e8bf
E-Hern Lee <somebody1234@users.noreply.github.com>
2017-10-09 18:05:58 +1000

    3.13.1

--- ppcg.user.js
+++ ppcg.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        PPCG Graduation Script
 // @namespace   https://github.com/vihanb/PPCG-Design
-// @version     3.13.0
+// @version     3.13.1
 // @description A script to self-graduate PPCG
 // @match       *://*.codegolf.stackexchange.com/*
 // @match       *://codegolf.meta.stackexchange.com/*
